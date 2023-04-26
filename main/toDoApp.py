84325c211b1469f036e398e93b7a5926de23b650
vivicarta <msaron@ivytech.edu>
2023-04-26 16:57:20 -0400

    Added the main input frame and some documentation.

--- main/toDoApp.py
+++ main/toDoApp.py
@@ -6,16 +6,21 @@ Purpose: This program is a GUI
 application of a simple To-Do List
 using Tkinter."""
 
-import tkinter as tk
+from tkinter import *
 
-root = tk.Tk()
+root = Tk()
 root.geometry("1050x750")
 root.title("Private To-Do's")
 
-header_label = tk.Label(root, text="To-Do List", font=("Arial", 35))
+# Set the header and subhead of the application
+header_label = Label(root, text="To-Do List", font=("Arial", 35))
 header_label.pack(padx=10, pady=(40, 10))
 
-subhead_label = tk.Label(root, text="Start your day productively.", font=("Arial", 14))
+subhead_label = Label(root, text="Start your day productively.", font=("Arial", 14))
 subhead_label.pack()
+
+# Add the input frame
+frame1 = Frame(root, width=750, height=280, bd=1, relief="solid", bg="white")
+frame1.pack(pady=40)
 
 root.mainloop()
