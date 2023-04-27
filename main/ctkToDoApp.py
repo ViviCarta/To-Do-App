588b271133855054a9cc2e5bee5cfebee36e5eb5
vivicarta <msaron@ivytech.edu>
2023-04-26 20:31:50 -0400

    Structured the ctk version into classes.

--- main/ctkToDoApp.py
+++ main/ctkToDoApp.py
@@ -8,20 +8,40 @@ using Custom Tkinter."""
 
 import customtkinter as ctk
 
-root = ctk.CTk()
-root.geometry("1050x750")
-root.title("Private To-Do's")
 
-first_header = ctk.CTkLabel(root, text="To-Do List", font=ctk.CTkFont("Arial", size=35))
-first_header.pack(padx=10, pady=(40, 10))
+class ListFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-second_header = ctk.CTkLabel(root, text="Start your day productively.", font=ctk.CTkFont("Arial", size=14))
-second_header.pack(pady=(5, 10))
+        # add widgets onto the frame...
+        # self.label = ctk.CTkLabel(self)
+        # self.label.grid(row=0, column=0, padx=20)
 
-scrollable_frame = ctk.CTkScrollableFrame(root, width=750, height=280)
-scrollable_frame.pack(pady=20)
 
-add_button = ctk.CTkButton(root, text="add task", width=50)
-add_button.pack(pady=20)
+class App(ctk.CTk):
+    """Displays the parent window."""
 
-root.mainloop()
+    def __init__(self):
+        super().__init__()
+        self.geometry("1050x750")
+        self.title("Private To-Do's")
+
+        """Add labels for main header and a subhead"""
+        self.heading = ctk.CTkLabel(self, text="To-Do List", font=ctk.CTkFont("Arial", size=35))
+        self.heading.pack(padx=10, pady=(40, 10))
+
+        self.subheading = ctk.CTkLabel(self, text="Start your day productively.",
+                                       font=ctk.CTkFont("Arial", size=14))
+        self.subheading.pack(pady=(5, 10))
+
+        """Create scrollable frame"""
+        self.scrollable_frame = ctk.CTkScrollableFrame(self, width=750, height=280)
+        self.scrollable_frame.pack(pady=20)
+
+        """Add a add_button"""
+        self.add_button = ctk.CTkButton(self, text="add task", width=50)
+        self.add_button.pack(pady=20)
+
+
+app = App()
+app.mainloop()
