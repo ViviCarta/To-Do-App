525bb7d8166e1450110915efceedea75729f3069
vivicarta <msaron@ivytech.edu>
2023-05-10 19:22:46 -0400

    Linked the login window and signup window.

--- main/ctkLogin.py
+++ main/ctkLogin.py
@@ -5,10 +5,140 @@ from tkinter import *
 from tkinter import messagebox
 import customtkinter as ctk
 from PIL import ImageTk, Image
+import ast
+
+
+class Signup(tkinter.Toplevel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.geometry("1000x750")
+        self.resizable(False, False)
+        self.title("Create New Account")
+
+        """Display a background image
+        for the sign up window"""
+        self.background_img = ImageTk.PhotoImage(Image.open("images/new.png"))
+        self.background_label = Label(self, image=self.background_img, bg="white")
+        self.background_label.pack()
+
+        """Create a border frame for 
+        label and entry widgets"""
+        self.border_frame = ctk.CTkFrame(self.background_label, width=500, height=570,
+                                         fg_color="white")
+        self.border_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        """Add main header and 
+        subhead for sign up page"""
+        self.login_heading = ctk.CTkLabel(self.border_frame, text="Create New Account",
+                                          font=ctk.CTkFont("Arial", size=40),
+                                          text_color="black")
+        self.login_heading.place(x=65, y=40)
+
+        self.login_subheading = ctk.CTkLabel(self.border_frame, text="Not yet registered? Sign up now.",
+                                             font=ctk.CTkFont("Arial", size=14),
+                                             text_color="black")
+        self.login_subheading.place(x=140, y=95)
+
+        # Create label and entry widgets
+        self.username_label = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
+                                           text="Username", text_color="black",
+                                           font=ctk.CTkFont("Arial", size=16))
+        self.username_label.place(x=120, y=155)
+        self.username_entry = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+                                           placeholder_text="name@example.com", placeholder_text_color="white",
+                                           font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
+                                           border_color="gray")
+        self.username_entry.place(x=120, y=190)
+
+        self.password_label1 = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
+                                            text="Password", text_color="black",
+                                            font=ctk.CTkFont("Arial", size=16))
+        self.password_label1.place(x=120, y=235)
+        self.password_entry1 = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+                                            placeholder_text="8-12 characters", placeholder_text_color="white",
+                                            font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
+                                            border_color="gray", show="•")
+        self.password_entry1.place(x=120, y=270)
+
+        self.password_label2 = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
+                                            text="Confirm Password", text_color="black",
+                                            font=ctk.CTkFont("Arial", size=16))
+        self.password_label2.place(x=120, y=320)
+        self.password_entry2 = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+                                            placeholder_text="Re-type your password", placeholder_text_color="white",
+                                            font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
+                                            border_color="gray", show="•")
+        self.password_entry2.place(x=120, y=355)
+
+        """Create a sign up button"""
+        self.signup_button = ctk.CTkButton(self.border_frame, width=140, text="Sign Up", corner_radius=6,
+                                           font=ctk.CTkFont("Arial", size=16), fg_color="black",
+                                           hover_color="gray", text_color="white",
+                                           command=self.signup)
+        self.signup_button.place(x=180, y=420)
+
+        """Create divider label
+        that separates the two
+        buttons"""
+        self.separator_label = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
+                                            text="Already have an account?", text_color="black",
+                                            font=ctk.CTkFont("Arial", size=14))
+        self.separator_label.place(x=170, y=475)
+
+        """Create a return to 
+        login button"""
+        self.return_button = ctk.CTkButton(self.border_frame, width=140, text="Return to Login", corner_radius=6,
+                                           font=ctk.CTkFont("Arial", size=16), fg_color="black",
+                                           hover_color="gray", text_color="white", command=self.return_login)
+        self.return_button.place(x=180, y=520)
+
+    def signup(self):
+        """Method for creating
+        an account"""
+        username = self.username_entry.get()
+        password = self.password_entry1.get()
+        confirm_pass = self.password_entry2.get()
+
+        if password == confirm_pass:
+            try:
+                """If file is available it will 
+                read the file and append data"""
+                file = open("datasheet", "r+")
+                d = file.read()
+                r = ast.literal_eval(d)
+
+                database = {username: password}
+                r.update(database)
+                file.truncate(0)
+                file.close()
+
+                file = open('datasheet', 'w')
+                file.write(str(r))
+
+                messagebox.showinfo("Sign Up", "Your account has been created.")
+
+            except:
+                """If file is unavailable, then
+                it will create the file"""
+                file = open('datasheet', 'w')
+                pp = str({'Username': 'Password'})
+                file.write(pp)
+                file.close()
+        else:
+            """Shows an error dialog box"""
+            messagebox.showerror("Invalid", "Both passwords don't match! Try again.")
+
+    def return_login(self):
+        """Method that will destroy
+        current window and open login
+        window"""
+        Signup.destroy(self)
 
 
 class ApplicationPage(tkinter.Toplevel):
     """Displays the main application page."""
+
     def __init__(self, master):
         super().__init__(master)
 
@@ -223,7 +353,7 @@ class Login(ctk.CTk):
         button"""
         self.signup_button = ctk.CTkButton(self.border_frame, width=140, text="Sign Up", corner_radius=6,
                                            font=ctk.CTkFont("Arial", size=14), fg_color="black",
-                                           hover_color="gray", text_color="white")
+                                           hover_color="gray", text_color="white", command=self.signup_command)
         self.signup_button.place(x=130, y=370)
 
     def show(self):
@@ -247,18 +377,25 @@ class Login(ctk.CTk):
         username = self.username_entry.get()
         password = self.password_entry.get()
 
-        if username == 'admin' and password == '1234':
+        file = open('datasheet', 'r')
+        d = file.read()
+        r = ast.literal_eval(d)
+        file.close()
+
+        # print(r.keys())
+        # print(r.values())
+
+        if username in r.keys() and password == r[username]:
             """Displays the main application window"""
             main_app = ApplicationPage(self)
             main_app.grab_set()
 
-        elif username == "" and password == "":
-            """Displays a warning dialog box"""
-            messagebox.showwarning("Warning", "Please don't leave any empty fields!")
-
-        elif username != 'admin' and password != '1234':
+        else:
             """Displays an error dialog box"""
             messagebox.showerror("Invalid", "Username and Password entered don't exist in the system.")
+
+    def signup_command(self):
+        signup_window = Signup()
 
 
 if __name__ == "__main__":
