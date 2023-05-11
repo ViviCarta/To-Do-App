3e4846ef3aa36918b77d3a7d937ceea75da93f45
vivicarta <msaron@ivytech.edu>
2023-05-10 20:10:07 -0400

    Added a hide/show button for each password entry.

--- main/ctkLogin.py
+++ main/ctkLogin.py
@@ -40,36 +40,57 @@ class Signup(tkinter.Toplevel):
                                              text_color="black")
         self.login_subheading.place(x=140, y=95)
 
-        # Create label and entry widgets
+        """This is a label and entry
+        widget for username"""
         self.username_label = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
                                            text="Username", text_color="black",
                                            font=ctk.CTkFont("Arial", size=16))
         self.username_label.place(x=120, y=155)
-        self.username_entry = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+        self.username_entry = ctk.CTkEntry(self.border_frame, width=250, height=35, fg_color="black",
                                            placeholder_text="name@example.com", placeholder_text_color="white",
                                            font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
                                            border_color="gray")
         self.username_entry.place(x=120, y=190)
 
+        """This is a label and entry
+        widget for first password"""
         self.password_label1 = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
                                             text="Password", text_color="black",
                                             font=ctk.CTkFont("Arial", size=16))
         self.password_label1.place(x=120, y=235)
-        self.password_entry1 = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+        self.password_entry1 = ctk.CTkEntry(self.border_frame, width=250, height=35, fg_color="black",
                                             placeholder_text="8-12 characters", placeholder_text_color="white",
                                             font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
                                             border_color="gray", show="•")
         self.password_entry1.place(x=120, y=270)
 
+        """Create a hide/show button
+        for the first password entry"""
+        hide_image1 = ctk.CTkImage(Image.open("images/hide.png").resize((32, 32)))
+        self.hide_button1 = ctk.CTkButton(self, image=hide_image1, text="", width=10,
+                                          anchor="center", fg_color="transparent", bg_color="black",
+                                          hover=False, command=self.show1)
+        self.hide_button1.place(x=570, y=363)
+
+        """This is a label and entry
+        widget for second password"""
         self.password_label2 = ctk.CTkLabel(self.border_frame, bg_color="transparent", fg_color="transparent",
                                             text="Confirm Password", text_color="black",
                                             font=ctk.CTkFont("Arial", size=16))
         self.password_label2.place(x=120, y=320)
-        self.password_entry2 = ctk.CTkEntry(self.border_frame, width=250, height=30, fg_color="black",
+        self.password_entry2 = ctk.CTkEntry(self.border_frame, width=250, height=35, fg_color="black",
                                             placeholder_text="Re-type your password", placeholder_text_color="white",
                                             font=ctk.CTkFont("Arial", size=13, slant="italic"), text_color="white",
                                             border_color="gray", show="•")
         self.password_entry2.place(x=120, y=355)
+
+        """Create a hide/show button
+        for the second password entry"""
+        hide_image2 = ctk.CTkImage(Image.open("images/hide.png").resize((32, 32)))
+        self.hide_button2 = ctk.CTkButton(self, image=hide_image2, text="", width=10,
+                                          anchor="center", fg_color="transparent", bg_color="black",
+                                          hover=False, command=self.show2)
+        self.hide_button2.place(x=570, y=448)
 
         """Create a sign up button"""
         self.signup_button = ctk.CTkButton(self.border_frame, width=140, text="Sign Up", corner_radius=6,
@@ -92,6 +113,34 @@ class Signup(tkinter.Toplevel):
                                            font=ctk.CTkFont("Arial", size=16), fg_color="black",
                                            hover_color="gray", text_color="white", command=self.return_login)
         self.return_button.place(x=180, y=520)
+
+    def show1(self):
+        """Method for showing
+        first password entry"""
+        show_image = ctk.CTkImage(Image.open("images/show.png").resize((32, 32)))
+        self.hide_button1.configure(image=show_image, command=self.hide1)
+        self.password_entry1.configure(show="")
+
+    def hide1(self):
+        """Method for hiding
+        first password entry"""
+        hide_image = ctk.CTkImage(Image.open("images/hide.png").resize((32, 32)))
+        self.hide_button1.configure(image=hide_image, command=self.show1)
+        self.password_entry1.configure(show="•")
+
+    def show2(self):
+        """Method for showing
+        second password entry"""
+        show_image = ctk.CTkImage(Image.open("images/show.png").resize((32, 32)))
+        self.hide_button2.configure(image=show_image, command=self.hide2)
+        self.password_entry2.configure(show="")
+
+    def hide2(self):
+        """Method for hiding
+        second password entry"""
+        hide_image = ctk.CTkImage(Image.open("images/hide.png").resize((32, 32)))
+        self.hide_button2.configure(image=hide_image, command=self.show2)
+        self.password_entry2.configure(show="•")
 
     def signup(self):
         """Method for creating
